ec018ad690dae993a203d8f6fb4cb372a95d1f01
Simon Ho <superkhau@gmail.com>
2015-08-11 23:02:29 -0700

    Relax id requirement for basic query operations

--- test/basic-querying.test.js
+++ test/basic-querying.test.js
@@ -620,6 +620,67 @@ describe('basic-querying', function () {
   });
 });
 
+describe('id field', function() {
+  var Todo;
+
+  before(function setupDb(done) {
+    var db = getSchema();
+    Todo = db.define('Todo', {
+      id: {type: 'number', id: false},
+      content: {type: 'string'}
+    });
+    db.automigrate(done);
+  });
+  beforeEach(function createFixtures(done) {
+    Todo.create([
+      {content: 'Buy eggs'},
+      {content: 'Buy milk'},
+      {content: 'Buy sausages'}
+    ], done);
+  });
+  afterEach(function deleteFixtures(done) {
+    Todo.destroyAll();
+    done();
+  });
+
+  it('should not be required for create', function(done) {
+    Todo.create({content: 'Buy ham'}, function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+
+  it('should not be required for delete', function(done) {
+    Todo.remove(function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('should not be required for find', function(done) {
+    Todo.find(function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('should not be required for findOne', function(done) {
+    Todo.findOne(function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+
+  it('should not be required for update', function(done) {
+    Todo.update({content: 'Buy ham'}, function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+});
+
 function seed(done) {
   var beatles = [
     {
